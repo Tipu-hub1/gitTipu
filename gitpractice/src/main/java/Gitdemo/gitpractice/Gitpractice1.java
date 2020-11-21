46ad425b78fb2edebeae91c1180643f55fc38167
bashiruddin <bashiruddin@localhost>
2020-11-21 18:23:59 -0500

    First Commit

---
+++ gitpractice/src/main/java/Gitdemo/gitpractice/Gitpractice1.java
@@ -0,0 +1,17 @@
+package Gitdemo.gitpractice;
+
+public class Gitpractice1 {
+
+	
+	//create new project
+	// create local git repository
+	//add file to staging
+	//commit changes to local repo(git)
+	// push changes to master branch remote git repo(github)
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+
+	}
+
+}
