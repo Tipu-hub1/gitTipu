4dd297da60b38c7a7117258997520417b2f539b0
bashiruddin <bashiruddin@localhost>
2020-11-21 21:22:34 -0500

    Second Commit

--- gitpractice/src/main/java/Gitdemo/gitpractice/Gitpractice1.java
+++ gitpractice/src/main/java/Gitdemo/gitpractice/Gitpractice1.java
@@ -10,6 +10,7 @@ public class Gitpractice1 {
 	// push changes to master branch remote git repo(github)
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("new Hello");
 
 
 	}
